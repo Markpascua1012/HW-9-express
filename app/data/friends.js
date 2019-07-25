2ea16161de70549c339b2135a14676244757c13f
Mark Pascua <markpascua1012@gmail.com>
2019-07-24 22:28:27 -0700

    almost everything set up, just need to work on the survey

---
+++ app/data/friends.js
@@ -0,0 +1,10 @@
+var friendArray = [
+    {
+        name: "Mark",
+        photo: "ahmed@example.com",
+        scores: [1,2,3,4,5,5,4,3,2,1]
+    }
+];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friendArray;
